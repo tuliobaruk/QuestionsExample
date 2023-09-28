065593a21eec40732c37a03905902bda13494445
Túlio Baruk <84753278+tuliobaruk@users.noreply.github.com>
2023-09-28 20:36:15 -0300

    fix: fix UnicodeDecodeError on windows os

--- importJsons.py
+++ importJsons.py
@@ -1,15 +1,13 @@
 import os
 import json
-
-# a bliblioteca pymongo é essencial para o script funcionar
 from pymongo import MongoClient
 
 # Conecte-se ao servidor MongoDB (certifique-se de que o servidor MongoDB esteja em execução)
 client = MongoClient('mongodb://localhost:27017/')
 
 # Acesse o banco de dados e a coleção onde você deseja importar os dados
-db = client['QuestionsAPI'] # O Nome do Banco de dados que será criado
-collection = db['Questions'] # O Nome da Collection que será criada
+db = client['QuestionsAPI']  # O Nome do Banco de dados que será criado
+collection = db['Questions']  # O Nome da Collection que será criada
 
 # Diretório onde os arquivos JSON estão localizados (pasta atual do arquivo .py)
 json_directory = os.path.dirname(__file__)
@@ -19,7 +17,7 @@ json_files = [f for f in os.listdir(json_directory) if f.endswith('.json')]
 
 # Itera sobre os arquivos .json e insere os dados na coleção
 for json_file in json_files:
-    with open(os.path.join(json_directory, json_file), 'r') as file:
+    with open(os.path.join(json_directory, json_file), 'r', encoding='utf-8') as file:
         data = json.load(file)
         collection.insert_many(data)
 
